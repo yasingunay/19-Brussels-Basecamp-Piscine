e83a2eb96e502aa7b2973976684c86129131278a
ygunay <ygunay@student.s19.be>
2021-09-07 22:31:22 +0000

    c00 1.part

---
+++ c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/07 21:59:16 by ygunay            #+#    #+#             */
+/*   Updated: 2021/09/07 22:00:01 by ygunay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+}
+
+/*
+int	main(void)
+{
+	ft_print_alphabet();
+}
+*/
---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/07 22:17:07 by ygunay            #+#    #+#             */
+/*   Updated: 2021/09/07 22:21:20 by ygunay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
+
+///*
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+}
+//*/
