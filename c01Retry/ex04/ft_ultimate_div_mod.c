f03d1418fe5148fe60ac81bdfe9bba6c8630af43
ygunay <ygunay@student.s19.be>
2021-09-11 17:33:28 +0000

    c01retry

---
+++ c01Retry/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/09 20:34:24 by ygunay            #+#    #+#             */
+/*   Updated: 2021/09/09 20:34:41 by ygunay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *a / *b;
+	*b = temp % *b;
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int	a;
+	int	b;
+
+	a = 10;
+	b = 3;
+	printf("%d, %d\n", a, b);
+	ft_ultimate_div_mod(&a, &b);
+	printf("%d, %d\n", a, b);
+	return (0);
+}
+*/
---
+++ c01Retry/ex07/ft_rev_int_tab.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/10 07:44:54 by ygunay            #+#    #+#             */
+/*   Updated: 2021/09/10 07:45:09 by ygunay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	tmp;
+
+	i = 0;
+	size = size - 1;
+	while (i < size)
+	{
+		tmp = tab[i];
+		tab[i] = tab[size];
+		tab[size] = tmp;
+		i++;
+		size--;
+	}
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int	tab[] = {0, 1, 2, 3, 4, 5};
+	int	size;
+	int	i;
+
+	size = 6;
+	i = 0;
+	ft_rev_int_tab(tab, size);	
+	while (i < 6)
+	{
+		printf("%d", tab[i]);
+		i++;
+	}
+	return (0);
+}
+*/
