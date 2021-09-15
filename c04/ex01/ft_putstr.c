71dcf9ca7b56e911bc6b7f678101fd2096e4ee92
ygunay <ygunay@student.s19.be>
2021-09-15 16:29:10 +0000

    ex00-03

---
+++ c04/ex01/ft_putstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/15 08:08:56 by ygunay            #+#    #+#             */
+/*   Updated: 2021/09/15 08:08:59 by ygunay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+/*
+int main()
+{
+	char s1[]="hello world";
+	ft_putstr(s1);
+	ft_putchar('\n');
+	return (0);
+}
+*/
