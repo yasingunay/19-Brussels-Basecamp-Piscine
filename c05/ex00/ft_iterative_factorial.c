450b44ee6b78371a37fcb13e8d9c17671514adb7
ygunay <ygunay@student.s19.be>
2021-09-17 09:34:59 +0000

    ex00-02

---
+++ c05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/16 18:49:45 by ygunay            #+#    #+#             */
+/*   Updated: 2021/09/16 18:49:47 by ygunay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{	
+	int	fact;
+
+	fact = 1;
+	if (nb < 0)
+		return (0);
+	while (nb != 0 )
+	{
+		fact = fact * nb;
+		nb--;
+	}
+	return (fact);
+}
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int nb= -10;
+    ft_iterative_factorial(nb);
+    printf("%d\n",ft_iterative_factorial(nb));
+    return 0;
+}
+*/
